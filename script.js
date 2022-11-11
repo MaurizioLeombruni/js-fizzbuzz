5f42e5b6f1d619b7a3191b97299037a29fdec6f9
MaurizioLeombruni <maurizio.leombruni@gmail.com>
2022-11-11 15:56:16 +0100

    Stesura codice JS, correzioni al codice CSS.

---
+++ script.js
@@ -0,0 +1,22 @@
+let squareContainer = document.querySelector(".square-container");
+
+for(let i=0; i<100; i++){
+
+//Controlliamo che il numero sia divisibile per 5. Se lo è, si controlla se è anche divisibile per 3, scrivendo FizzBuzz nel caso, scrivendo Buzz altrimenti.
+//Se il numero non è divisibile per 5, controlliamo se sia almeno divisibile per 3. Se lo è, scriviamo Fizz, altrimenti scriviamo semplicemente il numero.
+
+    if(i%5===0){
+
+        if(i%3===0){
+            squareContainer.innerHTML += '<div class="square-both"><p> ' + i + '<br>FizzBuzz</p></div>';
+        } else {
+            squareContainer.innerHTML += '<div class="square-buzz"><p> ' + i + '<br>Buzz</p></div>';
+        }
+
+    } else if(i%3===0){
+        squareContainer.innerHTML += '<div class="square-fizz"><p> ' + i + '<br>Fizz</p></div>'
+    } else {
+        squareContainer.innerHTML += '<div class="square-regular"><p> ' + i + '</p></div>';
+    }
+
+}
